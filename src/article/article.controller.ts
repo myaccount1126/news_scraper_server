4eb2a76adad14d88abfa60831a1315ed3753d5d7
WIN-F56DANN32VJ\PC <wws0120@gmail.com>
2024-08-18 23:20:20 +0800

    add scraping api endpoint

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Post } from '@nestjs/common';
 import { ArticleService } from './article.service';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articlesService: ArticleService) {}
+
+  @Post('run-cron')
+  async runCronJob() {
+    try {
+      await this.articlesService.handleCron();
+      return { message: 'Cron job has been triggered successfully.' };
+    } catch (error) {
+      // Log the error or throw it to be handled by NestJS's exception layer
+      return { message: 'Failed to run cron job.', error: error.message };
+    }
+  }
 }
