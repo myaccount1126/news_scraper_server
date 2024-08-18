138174ff4d60d2b09565928fa87ac0eacd130196
WIN-F56DANN32VJ\PC <wws0120@gmail.com>
2024-08-18 19:57:45 +0800

    add vercel.json file

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
-  console.log('app start on port 3001');
+  await app.listen(3000);
+  console.log('app start on port 3000');
 }
 bootstrap();
